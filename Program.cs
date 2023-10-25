cf78351125f057d76c2ae73fcc4f8ccf61168394
axmaijaz <asmaijaz456@gmail.com>
2023-10-25 13:48:42 +0500

    custome code to program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello ASMA! This is test app for Deployment");
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ASMA! This is test app for Deployment");
+        }
+    }
+}
